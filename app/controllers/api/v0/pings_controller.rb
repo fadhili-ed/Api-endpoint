664897a3e42fce03b41f0467dec62691def61e65
fadhili-ed <fadhilied00@gmail.com>
2020-05-12 10:22:46 +0300

    correct violations stated by hound

--- app/controllers/api/v0/pings_controller.rb
+++ app/controllers/api/v0/pings_controller.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
-class Api::V0::PingsController < ApplicationController
-  def index
-    render json: { message: 'Pong'}
+
+class Api
+  class V0
+    class PingsController < ApplicationController
+      def index
+        render json: { message: 'Pong'}
+      end
+    end
   end
 end
