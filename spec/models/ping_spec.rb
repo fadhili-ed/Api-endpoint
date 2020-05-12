a430465810e7e0f7e6a983ed4836ebfcf6ebae8a
fadhili-ed <fadhilied00@gmail.com>
2020-05-12 11:38:17 +0300

    correct violations stated by hound

--- spec/models/ping_spec.rb
+++ spec/models/ping_spec.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 require  'rails_helper'
 
 RSpec.describe Api::V0::PingsController, type: :request do
+
   describe 'GET /v0/ping' do
     it  'should return Ping' do
       get '/api/v0/pings'
