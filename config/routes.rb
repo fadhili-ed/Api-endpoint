9383092fd2d8de0d3b73ae75e0d20b6ba4f7a4e1
fadhili-ed <fadhilied00@gmail.com>
2020-05-13 16:28:00 +0300

    correct errors concerning rspec tests for user registration specifically on routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
+    namespace :v0 do
+      resources :pings, only: [:index], constraints: { format: 'json' }
+    end
     namespace :v1, default: { format: :json } do
       mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
     end
